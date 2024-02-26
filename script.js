d0a816a3c5402ea2f2faf3a40771ef157476f9d2
Muki Dev <mukidev881@gmail.com>
2024-02-26 16:30:52 +0000

    keypress event

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const userInput = document.getElementById('userInput');
 const enterButton = document.getElementById('button');
+const inputField = document.querySelector('input');
 const listElement = document.querySelector('ul');
 
 function checkInputLength(){
@@ -16,7 +17,14 @@ function addTaskAfterClick(){
     if(checkInputLength() > 0){
         createItemElement();
     }
-
 }
 
-enterButton.addEventListener('click',addTaskAfterClick)
+function addTaskAfterKeyPress(event){
+    if(checkInputLength() > 0 && event.which === 13){
+        createItemElement();
+    }
+}
+
+enterButton.addEventListener('click',addTaskAfterClick);
+
+inputField.addEventListener('keypress',addTaskAfterKeyPress);
